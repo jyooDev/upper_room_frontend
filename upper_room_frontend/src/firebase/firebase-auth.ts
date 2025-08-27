091277486a53b9e1a3770873a6ab0d42150ce54d
jyooDev <gina07099@gmail.com>
2025-08-27 11:04:06 -0500

    ✨ Add Firebase authentication utility functions
    feat: add firebase auth utilities for user creation, login, logout, and auth listener

--- upper_room_frontend/src/firebase/firebase-auth.ts
+++ upper_room_frontend/src/firebase/firebase-auth.ts
@@ -8,6 +8,10 @@ import {
 
 import { auth } from "../main";
 
+import Logger from "../utils/logger";
+
+const logger = new Logger("src/firebase/firebase-auth.ts");
+
 export const authListener = (callback: (user: User | null) => void) => {
   return onAuthStateChanged(auth, callback);
 };
@@ -17,7 +21,24 @@ export const createUser = async (email: string, password: string) => {
     const user = await createUserWithEmailAndPassword(auth, email, password);
     return user;
   } catch (error) {
-    console.error("src/firebase/firebase-auth.ts: createUser: error =", error);
+    logger.debug("createUser error = ", error);
     return null;
+  }
+};
+
+export const login = async (email: string, password: string) => {
+  try {
+    const user = await signInWithEmailAndPassword(auth, email, password);
+    return user;
+  } catch (error) {
+    logger.debug("login error = ", error);
+  }
+};
+
+export const signout = async () => {
+  try {
+    await signOut(auth);
+  } catch (error) {
+    logger.debug("logout error = ", error);
   }
 };
