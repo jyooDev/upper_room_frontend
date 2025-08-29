1bf1249125ab215be1d9268ac2b01e75b3ea1095
jyooDev <gina07099@gmail.com>
2025-08-28 22:20:30 -0500

    🩹 Minor UI fixes for navbar and refactor file names

---
+++ upper_room_frontend/src/pages/home.tsx
@@ -0,0 +1,14 @@
+import { Navbar, Hero, Feature, Contact, Community } from "../components";
+const Home = () => {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <Feature />
+      <Community />
+      <Contact />
+    </>
+  );
+};
+
+export default Home;
