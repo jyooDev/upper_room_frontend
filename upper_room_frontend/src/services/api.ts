a091ec8b39be63f6fe9b3fa2b6beb80739fc3271
jyooDev <gina07099@gmail.com>
2025-09-13 18:36:02 -0500

    ✨ Implement user id token in header of all users route

--- upper_room_frontend/src/services/api.ts
+++ upper_room_frontend/src/services/api.ts
@@ -1,10 +1,21 @@
 import axios from "axios";
 
 import config from "../config/config";
+import { getAuth } from "firebase/auth";
 
 export const usersApi = axios.create({
   baseURL: config.serverUrl + "/v1/users",
   headers: {
     "Content-Type": "application/json",
   },
+});
+
+usersApi.interceptors.request.use(async (config) => {
+  const auth = getAuth();
+  const user = auth.currentUser;
+  if (user) {
+    const token = await user.getIdToken();
+    config.headers["Authorization"] = `Bearer ${token}`;
+  }
+  return config;
 });
