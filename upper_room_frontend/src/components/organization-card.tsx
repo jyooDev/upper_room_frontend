d1e823d717abfaf41819ef6675060cc6fa81b6e4
jyooDev <gina07099@gmail.com>
2025-09-14 12:15:01 -0500

    🩹 Fix organizationcard props

--- upper_room_frontend/src/components/organization-card.tsx
+++ upper_room_frontend/src/components/organization-card.tsx
@@ -4,27 +4,52 @@ import { Button } from "@/components/ui/button";
 interface OrganizationCardProps {
   orgName: string;
   logoImage: string;
+  join?: boolean;
 }
 
-const OrganizationCard = ({ orgName, logoImage }: OrganizationCardProps) => {
+const OrganizationCard = ({
+  orgName,
+  logoImage,
+  join = false,
+}: OrganizationCardProps) => {
   return (
-    <Card className="p-3 w-70 h-70 flex flex-col justify-between items-center border-2 border-gray-300">
+    <Card
+      className={`p-3 ${
+        join ? "w-50 h-50" : "w-70 h-70"
+      }} flex flex-col justify-between items-center border-2 border-gray-300`}
+    >
       <CardContent>
         <img
-          className="w-40 h-40 rounded-full object-cover"
+          className={`${
+            join ? "w-full h-full" : "w-40 h-40"
+          } rounded-full object-cover`}
           src={logoImage}
           alt={orgName}
         />
       </CardContent>
       <span className="w-full border-b"></span>
       <CardFooter className="p-0 flex w-full justify-between items-center">
-        <p className="text-xs text-gray-700">{orgName}</p>
-        <Button
-          type="button"
-          className="bg-gray-700 hover:bg-gray-800 transition"
-        >
-          Org Page
-        </Button>
+        {join ? (
+          <>
+            <p className="text-xs text-gray-700">{orgName}</p>
+            <Button
+              type="button"
+              className="bg-gray-700 hover:bg-gray-800 transition"
+            >
+              Join
+            </Button>
+          </>
+        ) : (
+          <>
+            <p className="text-xs text-gray-700">{orgName}</p>
+            <Button
+              type="button"
+              className="bg-gray-700 hover:bg-gray-800 transition"
+            >
+              Org Page
+            </Button>
+          </>
+        )}
       </CardFooter>
     </Card>
   );
